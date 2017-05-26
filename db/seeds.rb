a2e53872b415d551e20036d299bdb2e5abc414d6
陈小敏 <chenxiaomin@chenxiaomins-MacBook-Pro.local>
2017-05-26 21:05:04 +0800

    delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,14 @@ u.password_confirmation = "123123"
 
 u.is_admin = true
 u.save
+
+
+products_categories = ["科技众筹产品", "科技众筹产品","科技众筹产品","科技众筹产品","科技众筹产品"]
+create_products = for i in 1..5 do
+Product.create!([title: products[i - 1],
+description: products_description[i - 1],
+quantity: rand(0..10),
+price: products_prices[i - 1],
+image: File.open(File.join(Rails.root, products_images[i-1])),
+categories: products_categories[i-1] ])
+end
