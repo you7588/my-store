b6a228a0840017716d8eb140c858267937e8b0b3
陈小敏 <chenxiaomin@chenxiaomins-MacBook-Pro.local>
2017-05-27 09:34:23 +0800

    fixed product#index

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -7,18 +7,27 @@ class Admin::ProductsController < ApplicationController
 
   def index
     @products = Product.all
+  #   if params[:category].blank?
+  #   @products = Product.where(is_hidden: false).order("created_at DESC")
+  # else
+  #   @category_id = Category.find_by(name: params[:category]).id
+  #   @products = Product.where(:category_id => @category_id).order("created_at DESC")
+  # end
   end
 
   def new
     @product = Product.new
+    @categories = Category.all.map { |c| [c.name, c.id] }
   end
 
   def edit
     @product = Product.find(params[:id])
+    @categories = Category.all.map { |c| [c.name, c.id] }
   end
 
   def update
     @product = Product.find(params[:id])
+    @product.category_id = params[:category_id]
 
     if @product.update(product_params)
       redirect_to admin_products_path
@@ -29,6 +38,7 @@ class Admin::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    @product.category_id = params[:category_id]
 
     if @product.save
       redirect_to admin_products_path
@@ -40,6 +50,6 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price, :image)
+    params.require(:product).permit(:title, :description, :quantity, :price, :image, :category_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516001305) do
+ActiveRecord::Schema.define(version: 20170526130730) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20170516001305) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -55,9 +61,12 @@ ActiveRecord::Schema.define(version: 20170516001305) do
     t.text     "description"
     t.integer  "quantity"
     t.integer  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "image"
+    t.string   "categories"
+    t.integer  "stars_number"
+    t.integer  "category_id"
   end
 
   create_table "users", force: :cascade do |t|
