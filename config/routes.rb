6babb5c0fd702d411ddc7c0ecbfc881b64b7bcfd
you7588 <yo7588@gmail.com>
2017-06-02 21:37:04 +0800

    product section detail

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'products#index'
 
   resources :products do
     member do
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   end
 
   resources :feedbacks
-  
+
   resources :carts do
     collection do
       delete :clean
