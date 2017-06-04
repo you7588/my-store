36b66ee14afb74118d8ef23e068efdfa0cab4eb5
you7588 <yo7588@gmail.com>
2017-06-04 16:18:25 +0800

    finish the commit-reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
 
   def show
       @product = Product.find(params[:id])
-      @reviews = @product.reviews.recent
+      @reviews = @product.reviews.recent.paginate(:page => params[:page], :per_page => 5)
       if @reviews.blank?
           @avg_experience = 0
           @avg_production = 0
