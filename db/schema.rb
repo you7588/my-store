de0ae1a72128e608f104b0b4fa6fb836229ad223
you7588 <yo7588@gmail.com>
2017-06-06 01:19:17 +0800

    check the wrong mailer that stop heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170605112104) do
+ActiveRecord::Schema.define(version: 20170605002137) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -41,26 +41,15 @@ ActiveRecord::Schema.define(version: 20170605112104) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "olds", force: :cascade do |t|
-    t.integer  "total",      default: 0
-    t.integer  "user_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
   create_table "orders", force: :cascade do |t|
-    t.integer  "total",            default: 0
+    t.integer  "total",          default: 0
     t.integer  "user_id"
-    t.string   "billing_name"
-    t.string   "billing_address"
-    t.string   "shipping_name"
-    t.string   "shipping_address"
-    t.datetime "created_at",                                null: false
-    t.datetime "updated_at",                                null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.string   "token"
-    t.boolean  "is_paid",          default: false
+    t.boolean  "is_paid",        default: false
     t.string   "payment_method"
-    t.string   "aasm_state",       default: "order_placed"
+    t.string   "aasm_state",     default: "order_placed"
     t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
 
