9300afdf5e51a4f1d48c0a4887f8f3ffc5da0b9f
you7588 <yo7588@gmail.com>
2017-06-05 10:30:16 +0800

    product#show lead to carts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,18 +18,18 @@ class ProductsController < ApplicationController
     end
 
     def add_to_cart
-  @product = Product.find(params[:id])
-  @quantity = params[:quantity].to_i
-  # 判断加入购物车的商品是否超过库存
+      @product = Product.find(params[:id])
+      @quantity = params[:quantity].to_i
+      # 判断加入购物车的商品是否超过库存
 
-  if @quantity > @product.quantity
-    @quantity = @product.quantity
-    flash[:warning] = "您选择的商品数量超过库存，实际加入购物车的商品为#{@quantity}件。"
-  else
-    current_cart.add(@product, @quantity)
-  end
-  redirect_to product_path(@product)
-end
+      if @quantity > @product.quantity
+        @quantity = @product.quantity
+        flash[:warning] = "您选择的商品数量超过库存，实际加入购物车的商品为#{@quantity}件。"
+      else
+        current_cart.add(@product, @quantity)
+      end
+      redirect_to carts_path
+    end
 
 
 end
