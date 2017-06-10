726ffa6b8438a68585992c2206edb9a2e80757f6
you7588 <yo7588@gmail.com>
2017-06-10 17:16:59 +0800

    change the photos size

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,11 +23,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
 
   version :thumb do
-    process resize_to_fill: [200, 200]
+    process resize_to_fill: [400, 200]
   end
 
   version :medium do
-    process resize_to_fill: [400,400]
+    process resize_to_fill: [1200,600]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
